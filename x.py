8b65c7e8f3ab22c3ad5c6fc645ce51038433a024
jrb28 <james.bradley@mason.wm.edu>
2019-07-26 09:13:07 -0400

    Revised files for 2019 boot camp

---
+++ x.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 26 06:40:29 2019
+
+@author: jrbrad
+"""
+
+x = 2
---
+++ y.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 26 06:41:26 2019
+
+@author: jrbrad
+"""
+
+y = 2*x
+print(y)
